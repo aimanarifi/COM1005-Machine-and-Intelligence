e69223cb874fac2acf25232b83e641ce389254da
aimanarifi <104199588+aimanarifi@users.noreply.github.com>
2022-04-22 22:13:04 +0100

    Update EpuzzleSearch.java

--- Assignment 3- 8 puzzle problem/searchEngine/BFS/EpuzzleSearch.java
+++ Assignment 3- 8 puzzle problem/searchEngine/BFS/EpuzzleSearch.java
@@ -1,4 +1,43 @@
+import java.util.Random;
 
 public class EpuzzleSearch extends Search {
-
+	
+	private EpuzzGen puzzleStarter;
+	private EpuzzleState puzzleState;
+	private int[][] puzzleLayout;
+	private int[][] target = puzzleStarter.tar;
+	private int difficulty = 0;
+	
+	//Constructor(s)
+	public EpuzzleSearch() {
+		Random rand = new Random();
+		while (difficulty < 6) {
+			difficulty = rand.nextInt(12);
+		}
+		
+		puzzleStarter = new EpuzzGen();
+		puzzleLayout = puzzleStarter.puzzGen(difficulty);
+	}
+	
+	public EpuzzleSearch(int seed) {
+		Random rand = new Random();
+		while (difficulty < 6) {
+			difficulty = rand.nextInt(12);
+		}
+		puzzleStarter = new EpuzzGen(seed);
+		puzzleLayout = puzzleStarter.puzzGen(difficulty);
+	}
+	
+	//Accessor(s)
+	public int[][] getTarget() {
+		return target;
+	}
+	
+	public EpuzzleState getPuzzleState(){
+		return puzzleState;
+	}
+	
+	public int[][] getPuzzleLayout(){
+		return getPuzzleState().getPuzzleLayout();
+	}
 }
