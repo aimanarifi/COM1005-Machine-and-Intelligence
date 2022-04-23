9494c0f0088df7c48f9bdee31197e5c0c5b7516d
aimanarifi <104199588+aimanarifi@users.noreply.github.com>
2022-04-23 19:29:50 +0100

    Update RunEpuzzleBFS.java
    Add some codes for it to run

--- Assignment 3- 8 puzzle problem/searchEngine/BFS/RunEpuzzleBFS.java
+++ Assignment 3- 8 puzzle problem/searchEngine/BFS/RunEpuzzleBFS.java
@@ -1,4 +1,19 @@
 
 public class RunEpuzzleBFS {
-
+	public static void main(String[] arg) {
+			
+		//Specify seed and diff
+		int seed = 1;
+		int difficulty = 6;
+		
+		//Search execution
+		EpuzzGen puzz = new EpuzzGen(seed);
+		
+		int[][] puzzleStartingLayout = puzz.puzzGen(difficulty);
+		EpuzzleState startingState = new EpuzzleState(puzzleStartingLayout);
+		EpuzzleSearch searcher = new EpuzzleSearch( puzzleStartingLayout, puzz.tar);
+		
+		//Print output
+		System.out.print(searcher.runSearch(startingState, "breadthFirst"));
+  }
 }
